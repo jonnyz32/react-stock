7850bac6caec9ebd48322a16c71709fd7de4c6c5
Jonathan Zak <Jonathan.Zak@ibm.com>
2023-10-10 23:44:29 -0400

    show dropdown menu

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@
 //alpha vantage api key 6KFEPWZX4P58N7VR
 // Second api key TN4REAKJA1FP8L3N
 
-import { useState } from 'react'
+import { useState, useMemo } from 'react'
 import moment from 'moment';
 import './App.css'
 import { useRef } from 'react';
@@ -19,18 +19,152 @@ import Watchlist from './Watchlist';
 import TextField from '@mui/material/TextField/TextField';
 import Button from '@mui/material/Button/Button';
 import { environment } from './environments';
+import {Trie} from './trie';
+import {nasdaq} from './../nasdaq';
+// import {amex} from '../amex';
+// import {tsx} from './../tsx';
+import {nyse} from './../nyse';
+import { Autocomplete, MenuItem } from '@mui/material';
+
+
+
 
 // interface ApiRes {
 //     "Time Series (Daily)": any
 // }
 
+
+
 const App = (props: HighchartsReact.Props) => {
-  const [data, setData] = useState<[number, number][]>([])
+  const trie = new Trie();
   const [currentStock, setCurrentStock] = useState<string>("");
+  const [data, setData] = useState<[number, number][]>([])
+
+
+
+  
+
+  const options2 = useMemo(() => {
+    const numberFormat = new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD', minimumFractionDigits: 0, maximumFractionDigits: 0});
+
+    const options = {
+      chart: {
+        height: '100%',
+        // events: {
+        //   load: function() {
+        //     const series = this.series[0];
+        //     const data = series.data;
+    
+        //     // Set the initial range to show the data at the end
+        //     this.xAxis[0].setExtremes(data[data.length - 10][0], data[data.length - 1][0]);
+        //   }
+        // }
+        // width: 600
+      },
+      // xAxis: {
+      //   events: {
+      //     afterSetExtremes: function(e) {
+      //       const chart = this.chart;
+      //       // Update the range to match the selected timeframe
+      //       const start = e.min;
+      //       const end = e.max;
+      //       chart.xAxis[0].setExtremes(start, end);
+      //     }
+      //   }
+      // },
+      yAxis: [{
+        opposite: false,  
+        labels: {
+          formatter: function (this) {
+            return numberFormat.format(this.value as number) 
+          },
+        },
+      },
+      ],
+      plotOptions: {
+        series: {
+          showInNavigator: true,
+          gapSize: 6,
+    
+        }
+      },
+    
+    
+      tooltip: {
+        shared: true,
+        formatter: function (this) {
+          return numberFormat.format(this.y as number) +  '</b><br/>' + moment(this.x).format('MMMM Do YYYY, h:mm')
+        }
+      },
+        title: {
+            text: currentStock
+        },
+        legend: {
+          enabled: true
+        },
+        rangeSelector: {
+          buttons: [{
+            type: 'day',
+            count: 1,
+            text: '1D',
+          }, {
+            type: 'day',
+            count: 5,
+            text: '5D'
+          }, {
+            type: 'month',
+            count: 1,
+            text: '1M'
+          }, {
+            type: 'month',
+            count: 6,
+            text: '6M'
+          },
+          {
+            type: 'year',
+            count: 1,
+            text: '1Y'
+          },
+          {
+            type: 'year',
+            count: 5,
+            text: '5Y'
+          },
+    
+            {
+            type: 'all',
+            text: 'Max'
+          }],
+          selected: 4
+        },
+        
+        series: [{
+            name: 'Price',
+            type: 'spline',
+            data: data
+        }],
+        credits: {
+          enabled: false
+        }
+    }
+    // Compute or get your options object here
+    return options;
+  }, [currentStock, data]);
+
+  // for (const index of [nasdaq, amex, tsx, nyse]){
+    for (const index of [nasdaq, nyse]){
+
+    for (const stock of index){
+      trie.insert(stock[0])
+    }
+  }
+
+  const [menuItems, setMenuItems] = useState<string[]>([])
+  // const [option, setOption] = useState(options)
+
   // const [watchListItems, setWatchListItems] = useState<[string, number, string, string][]>([["AAPL", -1, '0', '0']])
   const inputRef = useRef<HTMLInputElement>(null);
   // const watchListRef = useRef<HTMLInputElement>(null);
-  const numberFormat = new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD', minimumFractionDigits: 0, maximumFractionDigits: 0});
 // const APIKEY = 'TN4REAKJA1FP8L3N'
 
 const getData = async (ticker:string, size: 'full' | 'compact'):Promise<[number, number][]> => {
@@ -76,86 +210,15 @@ const getData = async (ticker:string, size: 'full' | 'compact'):Promise<[number,
     setCurrentStock(ticker)
   }
 
-  const options: Highcharts.Options = {
-    chart: {
-      height: '100%'
-      // width: 600
-    },
-    yAxis: [{
-      opposite: false,  
-      labels: {
-        formatter: function (this) {
-          return numberFormat.format(this.value as number) 
-        },
-      },
-    },
-    ],
-    plotOptions: {
-      series: {
-        showInNavigator: true,
-        gapSize: 6,
+  const setOptions = (prefix:string) =>{
+    if (prefix === ''){
+      setMenuItems([])
+    } else {
+      setMenuItems(trie.find(prefix.toUpperCase()))
+    }
+  }
+
   
-      }
-    },
-  
-  
-    tooltip: {
-      shared: true,
-      formatter: function (this) {
-        return numberFormat.format(this.y as number) +  '</b><br/>' + moment(this.x).format('MMMM Do YYYY, h:mm')
-      }
-    },
-      title: {
-          text: currentStock
-      },
-      legend: {
-        enabled: true
-      },
-      rangeSelector: {
-        buttons: [{
-          type: 'day',
-          count: 1,
-          text: '1D',
-        }, {
-          type: 'day',
-          count: 5,
-          text: '5D'
-        }, {
-          type: 'month',
-          count: 1,
-          text: '1M'
-        }, {
-          type: 'month',
-          count: 6,
-          text: '6M'
-        },
-        {
-          type: 'year',
-          count: 1,
-          text: '1Y'
-        },
-        {
-          type: 'year',
-          count: 5,
-          text: '5Y'
-        },
-  
-          {
-          type: 'all',
-          text: 'Max'
-        }],
-        selected: 4
-      },
-      
-      series: [{
-          name: 'Price',
-          type: 'spline',
-          data: data
-      }],
-      credits: {
-        enabled: false
-      }
-  };
   const chartComponentRef = useRef<HighchartsReact.RefObject>(null);
 
   return (
@@ -163,19 +226,36 @@ const getData = async (ticker:string, size: 'full' | 'compact'):Promise<[number,
           <h1>Enter a stock to search</h1>
     <form className='stockForm'>
     {/* <input className='stockSearch' ref={inputRef} placeholder='IBM'></input> */}
-    <TextField className='stockSearch' ref={inputRef} id="standard-basic" label="IBM" variant="standard" />
+    {/* <div className="inputContainer"> */}
+    <TextField onChange={(e)=>{setOptions(e.target.value)}} className='stockSearch' ref={inputRef} id="standard-basic" label="Enter a stock" variant="standard">
+    </TextField>
+    <div className='menuItemsOuterContainer'>
+      <div className='menuItemsInnerContainer'>
+      {menuItems.slice(0,5).map(item =>(<div onClick={()=>{
+        (inputRef.current!.children[1].children[0] as HTMLInputElement).value = item;
+        setMenuItems([])
+        }} className='stockMenuItem'>{item}</div>))}
+      </div>
+    </div>
+    {/* </div> */}
+    
+
     {/* <input className='stockSubmit' type='submit' onClick={(e)=>{
       e.preventDefault()
       updateStockChart(inputRef.current!.value)
       }}></input> */}
-      <Button variant="contained" type='submit' onClick={(e)=>{
+      <Button className="getStock" variant="contained" type='submit' onClick={(e)=>{
       e.preventDefault()
       // updateStockChart(inputRef.current!.value)
       updateStockChart((inputRef.current!.children[1].children[0] as HTMLInputElement).value)
+      setMenuItems([])
 
       }}>Submit</Button>
 
   </form>
+  {/* {menuItems.slice(0,5).map(item =>(<div className='stockMenuItem'>{item}</div>))} */}
+
+
   <div className='container'>
    
 
@@ -188,7 +268,7 @@ const getData = async (ticker:string, size: 'full' | 'compact'):Promise<[number,
     <HighchartsReact
       highcharts={HighchartsStk}
       constructorType={'stockChart'}
-      options={options}
+      options={options2}
       ref={chartComponentRef}
       {...props}
     />
